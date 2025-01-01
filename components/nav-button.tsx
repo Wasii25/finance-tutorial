4d3a3527d750510166df433955dade078c20cbca
Wasii25 <wasiullahrafeeq.s@gmail.com>
2025-01-01 23:54:41 +0530

    void-commit

--- components/nav-button.tsx
+++ components/nav-button.tsx
@@ -1,7 +1,6 @@
-import Link from "next/link";
-
 import { Button } from "./ui/button";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 type Props = {
  href: string;
@@ -19,7 +18,7 @@ export const NavButton = ({
             size="sm"
             variant="outline"
             className={cn(
-                "w-full lg:w-auto justify-between font-normal hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition",
+                "w-full lg:w-auto justify-between font-normal hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 outline-none text-white focus:bg-white/30 transition",
                 isActive ? "bg-white/10 text-white" : "bg-transparent",
             )}
         >
