7893af690ea6c797e5e45fc8d07924cc0691ff45
Wasii25 <wasiullahrafeeq.s@gmail.com>
2025-01-02 22:57:10 +0530

    Added the functionality for the add new account and create new account  in the account page and the account sheet respectively

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,38 +1,46 @@
 "use client"
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
+
+import { DataTable } from "@/components/data-table";
 
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { 
     Card,
     CardContent,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
-
 //classes for datatable
-import { Payment, columns } from "./columns"
-import { DataTable } from "@/components/data-table";
+import { columns } from "./columns"
 
 
-const data: Payment[] = [
-    {
-        id: "728ed52f",
-        amount: 100,
-        status: "pending",
-        email: "m@example.com",
-      },
-      {
-        id: "728ed52f",
-        amount: 50,
-        status: "success",
-        email: "a@example.com",
-      },
-]
 
 const AccountsPage = () => {
     const newAccouunt = useNewAccount();
+    const accountsQuery = useGetAccounts();
+    const accounts = accountsQuery.data || [];
+
+    if(accountsQuery.isLoading){
+        return (
+            <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+                <Card className="border-none drop-shadow-sm">
+                    <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+                        <Skeleton className="h-8 w-48"/>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="h-[500px] w-full flex items-center justify-center">
+                            <Loader2 className="size-6 text-slate-300 animate-spin"/>
+                        </div>
+                    </CardContent>
+                </Card>    
+            </div>
+        )
+    }
 
     return (
         <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -52,8 +60,8 @@ const AccountsPage = () => {
                 <CardContent>
                     <DataTable 
                     columns={columns} 
-                    data={data} 
-                    filterKey={"email"}
+                    data={accounts} 
+                    filterKey={"name"}
                     onDeleteAction={() => {}}
                     disabled={false}
                     />
